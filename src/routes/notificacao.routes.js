691ee129c9caaf3f0bb64be9c0f451ff6a1cd966
jhonyso <jhonysds2@gmail.com>
2025-09-17 00:31:37 -0300

    fix

--- src/routes/notificacao.routes.js
+++ src/routes/notificacao.routes.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const { criarNotificacao, listarNotificacoes, marcarComoLida } = require("../controllers/notificacao.controller");
-const { autenticar } = require("../middlewares/auth.middleware");
+const { autenticar, verificarPermissao } = require("../middlewares/auth.middleware");
 
 // Criar notificação (pode ser usado pelo sistema ao gerar eventos)
-router.post("/", autenticar, criarNotificacao);
+router.post("/", autenticar, verificarPermissao(['ADMIN', 'BARBEIRO']), criarNotificacao);
 
 // Listar notificações do usuário logado
-router.get("/", autenticar, listarNotificacoes);
+router.get("/", autenticar, verificarPermissao(['ADMIN', 'BARBEIRO']), listarNotificacoes);
 
 // Marcar notificação como lida
-router.patch("/:id/lida", autenticar, marcarComoLida);
+router.patch("/:id/lida", verificarPermissao(['ADMIN', 'BARBEIRO']), autenticar, marcarComoLida);
 
 module.exports = router;
