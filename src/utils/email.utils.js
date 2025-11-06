c9740d3c72a417be877971cab744d8ea443d4b02
weslley-souza <weskerdev95@gmail.com>
2025-11-05 21:31:50 -0300

    refactor: timiout sendemail

--- src/utils/email.utils.js
+++ src/utils/email.utils.js
@@ -11,10 +11,12 @@ export async function enviarEmailConfirmacao(email, token, urlBase) {
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
+    secure: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
+    connectionTimeout: 10000
   });
 
   const urlConfirmacao = `${urlBase}/confirm-email?token=${token}`;
